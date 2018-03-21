8189571e799d5df743e5e12e424cfcf8413fdbdc
Nett18 <kressckerl@gmail.com>
2018-03-21 08:34:28 +0100

    updated some tags

---
+++ js/gtag.js
@@ -0,0 +1,6 @@
+// Global site tag (gtag.js) - Google Analytics -->
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-112744820-4');
+gtag('set', {'user_id': 'USER_ID'}); // Indstil USER ID ved hjælp af det USER ID, der er logget ind.
